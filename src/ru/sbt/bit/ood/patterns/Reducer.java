44b4fc5c2b35f64f5e981854a753ace928ebc9b4
SBT-Agoshkov-IV <IVAgoshkov.SBT@sberbank.ru>
2016-10-31 11:19:37 +0300

    initial commit

---
+++ src/ru/sbt/bit/ood/patterns/Reducer.java
@@ -0,0 +1,17 @@
+package ru.sbt.bit.ood.patterns;
+
+public class Reducer {
+
+    public int reduce(int[] arr) {
+        if (arr.length == 1) return arr[0];
+        int[] reduced = new int[arr.length/2 + arr.length%2];
+        for (int i = 0;i < arr.length/2;i++) {
+            reduced[i] = arr[i*2] + arr[i*2+1];
+        }
+        if (arr.length % 2 == 1) {
+            reduced[reduced.length-1] = arr[arr.length-1];
+        }
+        return reduce(reduced);
+    }
+
+}
---
+++ test/ru/sbt/bit/odd/patterns/TestReducer.java
@@ -0,0 +1,28 @@
+package ru.sbt.bit.odd.patterns;
+
+import org.junit.Test;
+import ru.sbt.bit.ood.patterns.Reducer;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestReducer {
+
+    @Test
+    public void testReduceWorks() {
+        int[] arr = { -1, 2, 4, 0, 5 };
+        assertEquals( 10, new Reducer().reduce(arr));
+    }
+
+    @Test
+    public void testReduceWorksForTwoElements() {
+        int[] arr = { 1, 2 };
+        assertEquals( 3, new Reducer().reduce(arr));
+    }
+
+    @Test
+    public void testReduceWorksForOneElement() {
+        int[] arr = { -1 };
+        assertEquals( -1, new Reducer().reduce(arr));
+    }
+
+}
